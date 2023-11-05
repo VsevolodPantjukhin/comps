aa70a51c50daaeebca842c6712cace8aeec67b44
haki_ainz <seva.pantiukhin@yandex.ru>
2023-11-05 10:06:54 +0400

    Conditional Rendering

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -8,7 +8,7 @@ const Accordion = ({ items }) => {
     return (
       <div key={item.id}>
         <div>{item.label}</div>
-        <div>{item.content}</div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
