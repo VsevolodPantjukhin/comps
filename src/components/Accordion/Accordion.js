0e3a845a73c4afabba6bca8f1726f6b7bbfc4b1d
haki_ainz <seva.pantiukhin@yandex.ru>
2023-11-22 18:00:13 +0400

    Adding Styling

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -12,25 +12,28 @@ const Accordion = ({ items }) => {
     const isExpanded = index === expandedIndex;
 
     const icon = (
-      <span>{isExpanded ? <GoChevronDown /> : <GoChevronLeft />}</span>
+      <span className="text-2xl">
+        {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+      </span>
     );
 
     return (
       <div key={item.id}>
         <div
+          className="flex justify-between p-3 bg-gray-50 border-b items-center cursor-pointer"
           onClick={() => {
             handleClick(index);
           }}
         >
-          {icon}
           {item.label}
+          {icon}
         </div>
-        {isExpanded && <div>{item.content}</div>}
+        {isExpanded && <div className="border-b p-5">{item.content}</div>}
       </div>
     );
   });
 
-  return <div>{renderedItems}</div>;
+  return <div className="border-x border-t rounded">{renderedItems}</div>;
 };
 
 export default Accordion;
