96df203e569d1d7f7e6180be71a8ba94beba8282
haki_ainz <seva.pantiukhin@yandex.ru>
2023-11-22 17:39:26 +0400

    Inline Event Handlers

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -4,14 +4,20 @@ const Accordion = ({ items }) => {
   const [expandedIndex, setExpandedIndex] = useState(0);
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
-
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
+        <div
+          onClick={() => {
+            setExpandedIndex(index);
+          }}
+        >
+          {item.label}
+        </div>
         {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });
+
   return <div>{renderedItems}</div>;
 };
 
