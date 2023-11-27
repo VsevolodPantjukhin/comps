2eaff1accc22c32b8797c14b891b0a60ef9c8baa
haki_ainz <seva.pantiukhin@yandex.ru>
2023-11-27 14:56:42 +0400

    Navigation Context

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { NavigationProvider } from './context/navigation';
 import App from './App/App';
 import './index.css';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
-root.render(<App />);
+root.render(
+  <NavigationProvider>
+    <App />
+  </NavigationProvider>
+);
