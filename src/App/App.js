41e39aebd3700d51b84ef1cd0e4333204af32a25
haki_ainz <seva.pantiukhin@yandex.ru>
2023-11-27 14:39:02 +0400

    Answering Critical Questions

--- src/App/App.js
+++ src/App/App.js
@@ -1,30 +1,5 @@
-import Dropdown from '../components/Dropdown/Dropdown';
-import { useState } from 'react';
-
 const App = () => {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-
-  const options = [
-    {
-      label: 'Red',
-      value: 'red',
-    },
-    {
-      label: 'Green',
-      value: 'green',
-    },
-    {
-      label: 'Blue',
-      value: 'blue',
-    },
-  ];
-  return (
-    <Dropdown options={options} value={selection} onChange={handleSelect} />
-  );
+  return <div></div>;
 };
 
 export default App;
