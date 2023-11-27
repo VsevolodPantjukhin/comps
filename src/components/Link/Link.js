489b67bd8b1cb0df857a63b71f62746cd88832e1
haki_ainz <seva.pantiukhin@yandex.ru>
2023-11-27 15:20:00 +0400

    A Link Component

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,6 +1,14 @@
+import { useContext } from 'react';
+import NavigationContext from '../../context/navigation';
+
 const Link = ({ to, children }) => {
-  const handleClick = () => {
-    return;
+  const { navigate } = useContext(NavigationContext);
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    navigate(to);
   };
   return <a onClick={handleClick}>{children}</a>;
 };
+
+export default Link;
