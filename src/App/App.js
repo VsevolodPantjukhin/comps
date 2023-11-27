b6ed6dd5e09cfe94e727b44c8dfa01d2d7412608
haki_ainz <seva.pantiukhin@yandex.ru>
2023-11-27 15:28:12 +0400

    A Route Component

--- src/App/App.js
+++ src/App/App.js
@@ -1,10 +1,21 @@
 import Link from '../components/Link/Link';
+import Route from '../components/Route/Route';
+import AccordionPage from '../pages/AccordionPage';
+import DropdownPage from '../pages/DropdownPage';
 
 const App = () => {
   return (
     <div>
       <Link to="/accordion">Go to accordion</Link>
       <Link to="/dropdown">Go to dropdown</Link>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
     </div>
   );
 };
