2a0f568a8e8cc54c51438bdf199c136a6e8f697e
haki_ainz <seva.pantiukhin@yandex.ru>
2023-12-01 01:20:52 +0400

    At First Glance, Easy!

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,8 @@
 const Modal = () => {
   return (
-    <div className="modal">
-      <button>MODAL!</button>
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">I'm a modal</div>
     </div>
   );
 };
