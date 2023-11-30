dba2b865fa2fc952dab03813533e42b140e158a9
haki_ainz <seva.pantiukhin@yandex.ru>
2023-11-30 22:06:10 +0400

    Adding a Sidebar Component

--- src/App/App.js
+++ src/App/App.js
@@ -1,19 +1,22 @@
-import Link from '../components/Link/Link';
 import Route from '../components/Route/Route';
 import AccordionPage from '../pages/AccordionPage';
 import DropdownPage from '../pages/DropdownPage';
+import Sidebar from '../components/Sidebar/Sidebar';
+import ButtonPage from '../pages/ButtonPage';
 
 const App = () => {
   return (
-    <div>
-      <Link to="/accordion">Go to accordion</Link>
-      <Link to="/dropdown">Go to dropdown</Link>
+    <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
+      <Sidebar />
       <div>
         <Route path="/accordion">
           <AccordionPage />
         </Route>
-        <Route path="/dropdown">
+        <Route path="/">
           <DropdownPage />
+        </Route>
+        <Route path="/buttons">
+          <ButtonPage />
         </Route>
       </div>
     </div>
