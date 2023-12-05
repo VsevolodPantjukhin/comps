b9919f75b8a63dfdf38d98f7a8f567d63f0237f5
haki_ainz <seva.pantiukhin@yandex.ru>
2023-12-06 00:57:21 +0400

    Building the Rows

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,12 +1,23 @@
 const Table = ({ data }) => {
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr key={fruit.name}>
+        <td>{fruit.name}</td>
+        <td>{fruit.color}</td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
   return (
     <table>
       <thead>
-        <tr>Fruit</tr>
-        <tr>Color</tr>
-        <tr>Score</tr>
+        <tr>
+          <td>Fruit</td>
+          <td>Color</td>
+          <td>Score</td>
+        </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>{renderedRows}</tbody>
     </table>
   );
 };
