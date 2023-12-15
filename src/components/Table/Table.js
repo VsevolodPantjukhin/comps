837c700ff92c872f9206c581f5086306709388ca
haki_ainz <seva.pantiukhin@yandex.ru>
2023-12-15 23:09:52 +0400

    Adding a Key Function

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,15 +1,15 @@
-const Table = ({ data, config }) => {
+const Table = ({ data, config, keyFn }) => {
   const renderedHeaders = config.map((column) => {
     return <th key={column.label}>{column.label}</th>;
   });
-  const renderedRows = data.map((fruit) => {
+  const renderedRows = data.map((rowData) => {
+    const renderedCells = config.map((column) => {
+      return <td className="p-2">{column.render(rowData)}</td>;
+    });
+
     return (
-      <tr className="border-b" key={fruit.name}>
-        <td className="p-3">{config[0].render(fruit)}</td>
-        <td className="p-3">
-          <div className={`p-3 m-2 ${config[1].render(fruit)}`}></div>
-        </td>
-        <td className="p-3">{config[2].render(fruit)}</td>
+      <tr className="border-b" key={keyFn(rowData)}>
+        {renderedCells}
       </tr>
     );
   });
