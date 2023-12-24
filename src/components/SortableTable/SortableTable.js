b6543b3190fe71d9482eb9a5b8db5bd308d1c7a3
haki_ainz <seva.pantiukhin@yandex.ru>
2023-12-24 14:33:21 +0400

    Adding Sortable Table

---
+++ src/components/SortableTable/SortableTable.js
@@ -0,0 +1,19 @@
+import Table from '../Table/Table';
+
+const SortableTable = (props) => {
+  const { config } = props;
+
+  const updatedConfig = config.map((column) => {
+    if (!column.sortValue) {
+      return column;
+    }
+    return {
+      ...column,
+      header: () => <th>{column.label} IS SORTABLE</th>,
+    };
+  });
+
+  return <Table {...props} config={updatedConfig} />;
+};
+
+export default SortableTable;
